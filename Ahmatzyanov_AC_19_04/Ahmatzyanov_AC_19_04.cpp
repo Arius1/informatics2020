b013ef367c13dd05a2a70b0161aa19afb6ebf280
Arius <37404674+Arius1@users.noreply.github.com>
2020-09-16 15:22:44 +0300

    added struct pipe+KC
    added struct pipe+KC

--- Ahmatzyanov_AC_19_04/Ahmatzyanov_AC_19_04.cpp
+++ Ahmatzyanov_AC_19_04/Ahmatzyanov_AC_19_04.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+struct pipe {
+
+	string id;
+	float length;
+	int diameter;
+	bool repair_status;
+};
+
+struct KC {
+	
+	string id;
+	string Name;
+	int workshop_count;
+	int working_workshop_count;
+	double efficiency;
+
+};
+
 int main() {
-	cout << "Hello world" << endl;
+	setlocale(LC_ALL, "Russian");
+	cout << "Считывание данных для трубы: " << endl;
 
 	int ch;
 	cin >> ch;
